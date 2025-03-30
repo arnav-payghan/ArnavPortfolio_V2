19b2375b319c4656a79f85ab549e1187c63fef81
Arnav Payghan <arn.payghan@gmail.com>
2025-03-30 22:56:16 +0530

    Imported Neometric Extra Bold.

--- web-portfolio/src/pages/index.tsx
+++ web-portfolio/src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function IndexPage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title({size: "main"})}>arnav.</span>
+        <span className={`${title({ size: "main" })} neometric`}>arnav.</span>
         </div>
       </section>
     </DefaultLayout>
