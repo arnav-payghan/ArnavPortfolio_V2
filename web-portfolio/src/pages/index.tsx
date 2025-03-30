a582749e55fb4fd92d69c7e463cbb3a1646206c5
Arnav Payghan <arn.payghan@gmail.com>
2025-03-31 00:47:37 +0530

    Added Ghost Icon.

--- web-portfolio/src/pages/index.tsx
+++ web-portfolio/src/pages/index.tsx
@@ -5,17 +5,31 @@
 
 // import { siteConfig } from "@/config/site";
 import { title } from "@/components/primitives";
+import { useState } from "react";
 // import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
+  const [ghostImage, setGhostImage] = useState("/image-assets/ghostOpenEye.svg");
+
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-        <span className={`${title({ size: "main" })} neometric`}>arnav.</span>
-        </div>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 relative">
+        <div className="relative w-fit">
+          <img 
+            className="absolute -bottom-2 left-1/3 -translate-x-1/2 sm:-bottom-1 lg:-bottom-1
+            transition-transform duration-300 ease-in-out hover:scale-110 w-5 sm:w-12 md:w-20 lg:w-24 rounded-full z-10"
+            src={ghostImage}
+            onMouseEnter={() => setGhostImage("/image-assets/ghostClosedEye.svg")} 
+            onMouseLeave={() => setGhostImage("/image-assets/ghostOpenEye.svg")}
+            alt="Ghost"
+          />
+          <div className={`${title({ size: "main" })} neometric text-center relative z-1 text-base_Black`}>
+            arnav.
+          </div>
+        </div>  
       </section>
     </DefaultLayout>
   );
 }
+
--- web-portfolio/tailwind.config.js
+++ web-portfolio/tailwind.config.js
@@ -10,7 +10,15 @@ export default {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text_Black: "#080808",
+        text_White: "#000000",
+        base_Black: "#080808",
+        bg_White: "#F6F7FB",
+        primary_Blue: "#0057FF",
+      }
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
